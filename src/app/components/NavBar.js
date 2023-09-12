f9e5d7ca440924417e91a85d662284f398214e90
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-09-12 13:48:51 +0200

    feat: add image

--- src/app/components/NavBar.js
+++ src/app/components/NavBar.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Navbar = () => {
     return (
         <div className="z-20 mobile:px-10 lg:px-60 lg:text-[26px] lg:h-[80px] mobile:h-[70px] w-[100vw] flex text-5xl items-center justify-end font-normal shadow-md fixed nav-light txt-light">
-            
           <ul className="justify-center sm:flex hidden">
             <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Home</li>
             <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Get to know me</li>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,28 @@
-import NavBar from './components/NavBar'
+import NavBar from "./components/NavBar";
+import React from "react";
+import Image from "next/image";
+import ProfileImage from "./images/IMG_3722.jpg";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       <NavBar />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          New Teri&apos;s Coaching App
-        </p>
-       
+      <NavBar />
+      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      <div>
+        <div>
+          <Image
+            src={ProfileImage}
+            alt="ProfileImage"
+            width={500}
+            height={300}
+          />
+        </div>
+        <div>
+          <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            I am Terezie
+          </p>
+        </div>
       </div>
     </main>
-  )
+  );
 }
