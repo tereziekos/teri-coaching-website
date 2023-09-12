26e1c77e1629fc29323ea9d124674a74f6f52e4a
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-09-12 16:07:18 +0200

    fix: style

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -15,26 +15,33 @@ const Home = () => {
           />
         </div>
         <div>
-          <h1 className="  border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 dark:border-neutral-800 dark:bg-zinc-800/30 lg:static lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          <h1 className="">
             I am Terezie
           </h1>
           <p className="text-justify">
             I would be happy to help you. Be it with your inner exploration,
             relationships or practical problem.
           </p>
-          <h1>Book a coaching session with me</h1>
+         
         </div>
       </div>
-      <div>
-        <p className="text-justify">
-          “I can always choose, but I ought to know that if I do not choose, I
-          am still choosing.” Jean-Paul Sartre
-        </p>
-        <p className="text-justify">
-          “The only way to deal with an unfree world is to become so absolutely
-          free that your very existence is an act of rebellion.” ― Albert Camus
-        </p>
-
+      <div className="flex justify-center">
+        <div  >
+          <p>
+            “I can always choose, but I ought to know that if I do not choose, I
+            am still choosing.” Jean-Paul Sartre
+          </p>
+          <p>
+            “The only way to deal with an unfree world is to become so
+            absolutely free that your very existence is an act of rebellion.” ―
+            Albert Camus
+          </p>
+        </div>
+        <div >
+        <h1>Book a free intro coaching session with me</h1>
+          <p>Calendly</p>
+          <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
+        </div>
       </div>
     </div>
   );
