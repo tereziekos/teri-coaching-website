35aa38c48bfca6aacb085d118b722cd8a73eb1d5
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-09-12 15:19:27 +0200

    fea: styling

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div className="z-20 mobile:px-10 lg:px-60 lg:text-[26px] lg:h-[80px] mobile:h-[70px] w-[100vw] flex text-5xl items-center justify-end font-normal shadow-md fixed nav-light txt-light">
-          <ul className="justify-center sm:flex hidden">
-            <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Home</li>
-            <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Get to know me</li>
-            <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Coaching style</li>
-            <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">Contact me</li>
-          </ul>
-          
-        </div>
-      )
-}
+  return (
+    <div className="z-20 mobile:px-10 lg:px-60 lg:text-[26px] lg:h-[80px] mobile:h-[70px] w-[100vw] flex text-5xl items-center justify-end font-normal shadow-md fixed nav-light txt-light">
+      <div className="justify-items-start">
+        <h1 className="">Terezie Kosíková</h1>
+      </div>
+      <ul className="justify-center sm:flex hidden">
+        <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">
+          Home
+        </li>
+        <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">
+          Get to know me
+        </li>
+        <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">
+          Coaching style
+        </li>
+        <li className="list-none mx-5 hover:border-b-4 border-grey-600 cursor-pointer p-2 transition-all">
+          Contact me
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Navbar;
