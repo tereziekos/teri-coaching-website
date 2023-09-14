c4745956271f161ea5c606c64ae89e8c71730f27
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-09-14 14:08:44 +0200

    fix: style hompage

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -6,24 +6,28 @@ const Home = () => {
   return (
     <div className="z-10  grid grid-rows-2 grid-cols-2 gap-4 mt-32">
       <div className="row-span-1 col-span-1">
-        <Image src={ProfileImage} alt="ProfileImage" width={500} height={300} />
-        
+        <Image src={ProfileImage} alt="ProfileImage" />
       </div>
-      <div className="row-span-1 col-span-1">
-          <h1>I am Terezie</h1>
-          <p>
+      <div className="row-span-1 col-span-1 space-y-12">
+        <div>
+          <h1 className="text-center text-2xl mb-9">I am Terezie</h1>
+          <p className="text-center text-xl mb-11">
             I would be happy to help you. Be it with your inner exploration,
             relationships or practical problem.
           </p>
-          <p>
-          “I can always choose, but I ought to know that if I do not choose, I
-          am still choosing.” Jean-Paul Sartre
-        </p>
-        <p>
-          “The only way to deal with an unfree world is to become so absolutely
-          free that your very existence is an act of rebellion.” ― Albert Camus
-        </p>
         </div>
+        <div className="space-y-9">
+          <p className="text-lg text-white antialiased bg-gray-400 border border-gray-300 p-4 rounded">
+            “I can always choose, but I ought to know that if I do not choose, I
+            am still choosing.” <br /> Jean-Paul Sartre
+          </p>
+          <p className="text-lg  antialiased text-white bg-gray-400 border border-gray-300 p-4 rounded">
+            “The only way to deal with an unfree world is to become so
+            absolutely free that your very existence is an act of rebellion.”
+            <br/> Albert Camus
+          </p>
+        </div>
+      </div>
       <div className="row-span-1 col-span-2">
         <h1>Book a free intro coaching session with me</h1>
         <p>Calendly</p>
