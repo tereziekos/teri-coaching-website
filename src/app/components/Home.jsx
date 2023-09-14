9b9ee9f75b8b425ca2b9d38137e5cbac3bb492c9
Terezie Kosíková <89423234+tereziekos@users.noreply.github.com>
2023-09-14 11:22:24 +0200

    fix: layout home

--- src/app/components/Home.jsx
+++ src/app/components/Home.jsx
@@ -4,44 +4,36 @@ import ProfileImage from "/Users/terezka/work/website-Teri/teri_app/src/app/imag
 
 const Home = () => {
   return (
-    <div className="z-10 max-w-5xl w-full items-center mt-32">
-      <div className="flex justify-between items-center">
-        <div>
-          <Image
-            src={ProfileImage}
-            alt="ProfileImage"
-            width={500}
-            height={300}
-          />
-        </div>
-        <div>
-          <h1 className="">
-            I am Terezie
-          </h1>
-          <p className="text-justify">
+    <div className="z-10  grid grid-rows-2 grid-cols-2 gap-4 mt-32">
+      <div className="row-span-1 col-span-1">
+        <Image src={ProfileImage} alt="ProfileImage" width={500} height={300} />
+        
+      </div>
+      <div className="row-span-1 col-span-1">
+          <h1>I am Terezie</h1>
+          <p>
             I would be happy to help you. Be it with your inner exploration,
             relationships or practical problem.
           </p>
-         
         </div>
+      <div className="row-span-1 col-span-1">
+        <p>
+          “I can always choose, but I ought to know that if I do not choose, I
+          am still choosing.” Jean-Paul Sartre
+        </p>
+        <p>
+          “The only way to deal with an unfree world is to become so absolutely
+          free that your very existence is an act of rebellion.” ― Albert Camus
+        </p>
       </div>
-      <div className="flex justify-center">
-        <div  >
-          <p>
-            “I can always choose, but I ought to know that if I do not choose, I
-            am still choosing.” Jean-Paul Sartre
-          </p>
-          <p>
-            “The only way to deal with an unfree world is to become so
-            absolutely free that your very existence is an act of rebellion.” ―
-            Albert Camus
-          </p>
-        </div>
-        <div >
+      <div className="row-span-1 col-span-1">
         <h1>Book a free intro coaching session with me</h1>
-          <p>Calendly</p>
-          <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js" async></script>
-        </div>
+        <p>Calendly</p>
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async
+        ></script>
       </div>
     </div>
   );
